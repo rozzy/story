f73f951e028f0d4c8f0b6f8921dccac3c7d262df
Rozzy <berozzy@gmail.com>
2013-11-05 21:21:00 +0200

    remove db

--- lib/story/router.rb
+++ lib/story/router.rb
@@ -1,12 +1,7 @@
 module Story
   module Router
     def self.included request
-      request.get '/' do 
-        p Post.count.to_s
-
-        p = Post.new title: '12', body: '23'
-        p.save
-      end
+      
     end
   end
 end
