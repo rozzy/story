8c5d7266ce6096314b59b0675e31224c02f2fbec
Rozzy <berozzy@gmail.com>
2013-12-08 17:05:29 +0200

    Additional styles

--- lib/story.rb
+++ lib/story.rb
@@ -2,12 +2,6 @@ require 'story/meta'
 require 'sinatra'
 
 module Story
-  def self.lib_dir
-    t = ["#{File.dirname(File.expand_path($0))}/../lib/#{Meta::NAME}", "#{Gem.dir}/gems/#{Meta::NAME}-#{Meta::VERSION}/lib/#{Meta::NAME}"]
-    t.each {|i| return i if File.readable?(i) }
-    raise "both paths are invalid: #{t}"
-  end
-
   class Base < Sinatra::Base
     configure do
       set :views, settings.views.to_s.gsub(/views$/, 'story/templates/story')
@@ -15,8 +9,24 @@ module Story
       set :charset, Meta::CHARSET
     end
 
+    def load_additional_styles
+      begin
+        settings.additional_stylesheets
+        additional_styles = true
+      rescue NoMethodError
+      ensure
+        additional_styles = false
+      end
+    end
+
     get '/' do
       slim :index
     end
+  end
+
+  def self.lib_dir
+    ["#{File.dirname(File.expand_path($0))}/../lib/#{Meta::NAME}", "#{Gem.dir}/gems/#{Meta::NAME}-#{Meta::VERSION}/lib/#{Meta::NAME}"]
+    .each {|i| return i if File.readable?(i) }
+    raise LoadError
   end
 end
--- lib/story/meta.rb
+++ lib/story/meta.rb
@@ -3,6 +3,6 @@ module Story
     NAME = 'story'
     VERSION = '0.0.1'
     CHARSET = 'utf-8'
-    DEFAULT_BLOG_TITLE = 'Story Blog 2'
+    DEFAULT_BLOG_TITLE = 'Personal Blog'
   end
 end
