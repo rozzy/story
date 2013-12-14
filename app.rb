0eda16af799a8f431ea312bf0ed284569993f648
Rozzy <berozzy@gmail.com>
2013-12-14 13:02:26 +0200

    Deploying to Heroku

--- app.rb
+++ app.rb
@@ -6,12 +6,18 @@ class MyBlog < Story::Base
     set :styles_path, "styles"
     set :blog_title, 'Rozzy Blog'
     set :static_ext, ["txt", "md"]
+    set :title_separator, ": "
   end
 
   get '/:file.css' do |file|
     return parse_file file, "css", false if File.exists? "#{file}.css"
     return sass :"#{settings.styles_path}/#{file}" if File.exists? "#{settings.views}/#{settings.styles_path}/#{file}.sass"
     raise not_found
+  end
+
+  get '/tt' do
+    title "All posts"
+    slim :index
   end
 end
 
